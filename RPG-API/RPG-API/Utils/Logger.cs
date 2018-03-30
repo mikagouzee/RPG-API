6d7d0edee56b4a7c40338ba76242c91a5e9ffb4f
Michel Gouzee <mgouzee@interparking.com>
2018-03-30 11:44:40 +0200

    better logger now takes date into account

--- RPG-API/RPG-API/Utils/Logger.cs
+++ RPG-API/RPG-API/Utils/Logger.cs
@@ -8,12 +8,28 @@ namespace RPG_API.Utils
     {
         private string logFile = HttpContext.Current.Server.MapPath("~/App_Data/Logs/log.txt");
 
+        void Setup()
+        {
+            if (!File.Exists(logFile))
+            {
+                File.Create(logFile);
+            }
+            else
+            {
+                DateTime dt = File.GetLastWriteTime(logFile);
+                if (dt.Day != DateTime.Now.Day)
+                    logFile.Replace(".txt", "-" + DateTime.Now.Day + ".txt");
+            }
+        }
+
         public void Log(string description)
         {
+            Setup();
             using (StreamWriter file = new StreamWriter(@logFile, true))
             {
                 file.WriteLine(DateTime.Now.ToString() + " : " + description);
             }
+            File.SetLastWriteTime(logFile, DateTime.Now);
         }
     }
 }
